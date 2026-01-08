e51beeb052e8ad411446338f282fcde547da373a
rpopic2 <8055265+rpopic2@users.noreply.github.com>
2026-01-08 21:27:50 +0900

    made hashmap generic

--- alc-v2/hashmap.h
+++ alc-v2/hashmap.h
@@ -1,69 +1,67 @@
 #pragma once
 
-#include "emit.h"
-#include "err.h"
-
-typedef struct {
-    str key;
-    symbol_t value;
-} hashmap_entry;
-
-#define array_len ('Z' - 'A' + 1)
-typedef hashmap_entry hashmap[array_len];
-
-hashmap fn_ids;
-
+#include "str.h"
 int hashmap_hash(str id) {
     int index = id.data[0] - 'A';
     return index;
 }
 
-bool hashmap_entry_valid(const hashmap_entry *entry) {
-    return entry->key.data;
-}
+#define HASHMAP_GENERIC(T, array_len) \
+ \
+typedef struct { \
+    str key; \
+    T value; \
+} hashmap_entry_##T; \
+ \
+ \
+typedef hashmap_entry_##T hashmap_##T[array_len]; \
+ \
+bool hashmap_entry_##T##_valid(const hashmap_entry_##T *entry) { \
+    return entry->key.data; \
+} \
+ \
+inline static hashmap_entry_##T *hashmap_##T##_find(hashmap_##T self, const str id) { \
+    int index = hashmap_hash(id) % array_len; \
+    str_printdnl(&id), printd(" -> hash was: %d\n", index); \
+    int start = index; \
+ \
+    while (hashmap_entry_##T##_valid(&self[index])) { \
+        if (str_eq(self[index].key, id)) { \
+            break; \
+        } \
+        printd("linear probe\n"); \
+        index += 1; \
+        index %= array_len; \
+        if (index == start) { \
+            fprintf(stderr, "hash tabel is full!"); \
+            abort(); \
+        } \
+    } \
+    return &self[index]; \
+} \
+ \
+inline static hashmap_entry_##T *hashmap_##T##_tryfind(hashmap_##T self, const str s) { \
+    hashmap_entry_##T *entry = hashmap_##T##_find(self, s); \
+    if (hashmap_entry_##T##_valid(entry)) \
+        return entry; \
+    else \
+        return NULL; \
+} \
+ \
+inline static T *hashmap_##T##_overwrite(hashmap_##T self, str id, const T *value) { \
+    hashmap_entry_##T *entry = hashmap_##T##_find(self, id); \
+    entry->key = id, entry->value = *value; \
+    return &entry->value; \
+} \
+ \
+inline static T *hashmap_##T##_tryadd(hashmap_##T self, str id, const T *value) { \
+    hashmap_entry_##T *entry = hashmap_##T##_find(self, id); \
+    if (hashmap_entry_##T##_valid(entry)) { \
+        return NULL; \
+    } else { \
+        entry->key = id, entry->value = *value; \
+    } \
+    return &entry->value; \
+} \
+ \
 
-inline static hashmap_entry *hashmap_find(hashmap self, const str id) {
-    int index = hashmap_hash(id) % array_len;
-    str_printdnl(&id), printd(" -> hash was: %d\n", index);
-    int start = index;
-
-    while (hashmap_entry_valid(&self[index])) {
-        if (str_eq(self[index].key, id)) {
-            break;
-        }
-        printd("linear probe\n");
-        index += 1;
-        index %= array_len;
-        if (index == start) {
-            fprintf(stderr, "hash tabel is full!");
-            abort();
-        }
-    }
-    return &self[index];
-}
-
-inline static hashmap_entry *hashmap_tryfind(hashmap self, const str s) {
-    hashmap_entry *entry = hashmap_find(self, s);
-    if (hashmap_entry_valid(entry))
-        return entry;
-    else
-        return NULL;
-}
-
-inline static symbol_t *hashmap_overwrite(hashmap self, str id, const symbol_t *value) {
-    hashmap_entry *entry = hashmap_find(self, id);
-    entry->key = id, entry->value = *value;
-    return &entry->value;
-}
-
-inline static symbol_t *hashmap_tryadd(hashmap self, str id, const symbol_t *value) {
-    hashmap_entry *entry = hashmap_find(self, id);
-    if (hashmap_entry_valid(entry)) {
-        return NULL;
-    } else {
-        entry->key = id, entry->value = *value;
-    }
-    return &entry->value;
-}
-
-#undef array_len
