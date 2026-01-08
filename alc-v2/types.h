5c8b6eb92d32363ffd620c34752b2957309866c4
rpopic2 <8055265+rpopic2@users.noreply.github.com>
2026-01-08 22:31:20 +0900

    add more types

--- alc-v2/types.h
+++ alc-v2/types.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdint.h>
+#include <stddef.h>
 
 typedef enum {
     RD_NONE, RET, PARAM, SCRATCH, NREG, STACK, FRAME
@@ -16,11 +17,16 @@ typedef struct {
 } reg_t;
 
 typedef uint8_t u8;
-typedef int8_t i8;
-typedef int32_t i32;
-typedef int64_t i64;
+typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
+typedef size_t usize;
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+typedef ptrdiff_t isize;
 
 #define bool _Bool
 #define unreachable (printf("unreachable %s:%s:%d\n", __FILE__, __func__, __LINE__), abort())
